da8de9bf8d4de3ea91994500035809c1258d315d
Ágatha Ferreira <agathafs0908@gmail.com>
2024-09-09 11:35:46 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal é uma skatista brasileira, a mais jovem medalhista olímpica da história do Brasil. Conhecida como Fada, ela conquistou o mundo com seus movimentos ágeis e criativos.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags:"skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024, sendo a primeira brasileira a conquistar o ouro no judô em uma Olimpíada. Sua força e técnica impressionam a todos.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judô judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que fez história nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro no individual geral e outras medalhas. Sua versatilidade e determinação são inspiradoras.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ginástica ouro"
+    }
+]; 
